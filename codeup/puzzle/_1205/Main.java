813e0a7ae60fcc82e31c4e428a9543badc61d10d
TJ-BU-706-P11 <TJ-BU-706-P11@DESKTOP-4E6M7BI>
2025-03-13 18:17:35 +0900

    55

--- codeup/puzzle/_1205/Main.java
+++ codeup/puzzle/_1205/Main.java
@@ -16,12 +16,7 @@ public class Main {
 		arr[2] = a - b; arr[3] = b - a;
 		arr[4] = a * b; arr[5] = b * a;
 		arr[6] = a / b; arr[7] = b / a;
-		arr[8] = a ^ b; 
-		arr[9] = b ^ a;
-		
-		
-			
-		
+		arr[8] = a ^ b; arr[9] = b ^ a;
 		
 		double max = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length; i++) {
