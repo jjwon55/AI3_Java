236d4eb19b88f7c2e8f12bea1b782d516fc2e976
TJ-BU-706-P11 <TJ-BU-706-P11@DESKTOP-4E6M7BI>
2025-03-13 18:20:29 +0900

    55

--- codeup/puzzle/_1205/Main.java
+++ codeup/puzzle/_1205/Main.java
@@ -9,14 +9,15 @@ public class Main {
 		System.out.print("");
 			int a = sc.nextInt();
 			int b = sc.nextInt();
-		int arr[];	
-		arr = new int[10];
+		double arr[];	
+		arr = new double[10];
 		
 		arr[0] = a + b; arr[1] = b + a;
 		arr[2] = a - b; arr[3] = b - a;
 		arr[4] = a * b; arr[5] = b * a;
 		arr[6] = a / b; arr[7] = b / a;
-		arr[8] = a ^ b; arr[9] = b ^ a;
+		arr[8] = Math.pow(a, b);  // a의 b제곱
+        arr[9] = Math.pow(b, a);  // b의 a제곱
 		
 		double max = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length; i++) {
