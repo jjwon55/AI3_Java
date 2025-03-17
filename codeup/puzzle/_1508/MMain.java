1c7d8751b002ab3ed9c5e809a523833f954f3438
bomeo <bomeo@DESKTOP-MDJBPGP>
2025-03-18 00:13:09 +0900

    25

--- codeup/puzzle/_1508/MMain.java
+++ codeup/puzzle/_1508/MMain.java
@@ -18,11 +18,8 @@ public class MMain {
 		}
 		for (int i = 1; i < arr.length; i++) {
 			for (int j = 1; j < arr[i].length; j++) {
-<<<<<<< HEAD
 				arr[i][j] = arr[i][j - 1] - arr[j - 1][i];
-=======
 				arr[i][j] = arr[i][j - 1] - 1;
->>>>>>> branch 'main' of https://github.com/jjwon55/AI3_Java.git
 			}
 		}
 				
