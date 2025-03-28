0892878e28af655cbc205a6064c8801d46a87ca7
TJ-BU-706-P11 <TJ-BU-706-P11@DESKTOP-4E6M7BI>
2025-03-28 17:54:32 +0900

    123

---
+++ codeup/puzzle/_09/Main.java
@@ -0,0 +1,24 @@
+package _09;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("제니 ");
+		int n = sc.nextInt();
+		System.out.print("지수 ");
+		int n2 = sc.nextInt();
+		System.out.print("리사 ");
+		int n3 = sc.nextInt();
+		System.out.print("로제 ");
+		int n4 = sc.nextInt();
+		
+		System.out.println("성명 출장비 오만원 만원 오천원 천원 오백원 백원 오십원 십원 오원 일원");
+		int money = 50000;
+		int count = 0;
+		boolean a = true;
+		
+	}
+}
