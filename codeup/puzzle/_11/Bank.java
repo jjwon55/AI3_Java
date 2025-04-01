4fa3e751747246fe959b7240367e66b1c78dd3dd
TJ-BU-706-P11 <TJ-BU-706-P11@DESKTOP-4E6M7BI>
2025-04-01 17:01:09 +0900

    222

--- codeup/puzzle/_11/Bank.java
+++ codeup/puzzle/_11/Bank.java
@@ -1,5 +1,12 @@
 package _11;
 
+import java.util.Scanner;
+
 public class Bank {
 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+	}
 }
